7bc39f18bd35bc4e1bbaa0201675fac5214849e7
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-18 22:56:13 +0530

    Terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "roboshop" {
+  ami           = var.ami_id  #In variables we declared ami_id, so we given reference here
+  instance_type = var.environment == "dev" ? "t3.micro" : "t2.micro"  #Dont change right side name because it is the syntax in terraform, left side user defined.
+  vpc_security_group_ids = [ aws_security_group.allowing_all.id ] 
+
+  tags = var.ec2-tags
+}
+
+resource "aws_security_group" "allowing_all" {  #allowing all is tag nme
+    name        = "allow_all" #SG name
+    description = "allow all traffic"
+
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = var.sg_tags
+}
