e3a918e5e208966dcf666c32e7738a181cebee66
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-21 00:22:12 +0530

    terraform

--- state-s3/provider.tf
+++ state-s3/provider.tf
@@ -10,15 +10,17 @@ terraform {
     bucket = "devops84s-remote-state" #bucket we created in aws console/s3
     key    = "remote-state-demo"
     region = "us-east-1"
-    encrypt = true
-    use_lockfile = true
+    encrypt = true #enabling encryption
+    use_lockfile = true #enabling s3 native locking
     #dynamodb_table = "84s-remote-state" #Any name we can give.There is no rule that it should be match with bucket name.
   }
 }
 
-#Here state will be saved in bucket and for locking use dynamoDB. By using key name state file will be stored.
-#after giving terraform appy -auto-approve after its staart executing we can seee some entry in dynamoDB that meaans it is locking
-#after successful completion of terraform apply command we can see lock file in s3 bucket
+/* If we use encrypt and use_lockfile then no need to use dynamoDB, because use_lockfile is native locing methodolgy for S3 */
+
+/* Here state will be saved in bucket and for locking use dynamoDB. By using key name state file will be stored.
+after giving terraform appy -auto-approve after its staart executing we can seee some entry in dynamoDB that meaans it is locking
+after successful completion of terraform apply command we can see lock file in s3 bucket. */
 
 provider "aws" {
   # Configuration options here we have to mention asw credentiasl i.e, aws cli but it not recommended to provide creds here
